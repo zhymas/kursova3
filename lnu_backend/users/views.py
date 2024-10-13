7ef30b44d6d3918bb610574016b0053b570e729b
zhymas <zhymas@MacBook-Pro-Zimas.local>
2024-10-13 22:40:56 +0300

    Test deploy

--- lnu_backend/users/views.py
+++ lnu_backend/users/views.py
@@ -5,4 +5,5 @@ from rest_framework import status
 class HomeResponse(APIView):
     
     def get(self, request):
-        return Response({"status": True, "message": "I reject your sadness"}, status=status.HTTP_200_OK)
+        return Response({"status": True, "message": "I reject your sadness"}, 
+                        status=status.HTTP_200_OK)
